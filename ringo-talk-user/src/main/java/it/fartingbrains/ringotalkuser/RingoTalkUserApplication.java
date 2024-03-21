fc4d5d8e03437878f779ad576744bf63c7d2e259
Shin <sgroi.andrea89@gmail.com>
2024-03-21 23:53:51 +0100

    first push of the spring project

---
+++ ringo-talk-user/src/main/java/it/fartingbrains/ringotalkuser/RingoTalkUserApplication.java
@@ -0,0 +1,13 @@
+package it.fartingbrains.ringotalkuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingoTalkUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingoTalkUserApplication.class, args);
+	}
+
+}
---
+++ ringo-talk-user/src/test/java/it/fartingbrains/ringotalkuser/RingoTalkUserApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fartingbrains.ringotalkuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingoTalkUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
